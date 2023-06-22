2100e5ffcdd319377dc52ed1a6c3dbd545477d40
Cohen <itamar2.cohen@intel.com>
2023-06-22 15:10:00 +0300

    pictures

--- server/pictures.js
+++ server/pictures.js
@@ -6,12 +6,47 @@ router.get('/photos', (req, res) => {
     try{
         const albumID = req.query.albumId;
         const index = req.query._start;
-        const picture = picturesDB.getPictureByAlbumId(albumID, index)
-        res.send(picture);
+        const pictures = picturesDB.getPictureByAlbumId(albumID)
+        res.send(pictures[index]);
     }
     catch (error) {
         res.status(400).send({error: error.message})
     }
 })
+
+router.get('/photos/amount', (req, res) => {
+    try{
+        const albumID = req.query.albumId;
+        const pictures = picturesDB.getPictureByAlbumId(albumID)
+        res.send({amount: pictures.length});
+    }
+    catch (error) {
+        res.status(400).send({error: error.message})
+    }
+})
+
+router.put('/photos/:id', (req, res) => {
+    try{
+        const id = req.params.id;
+        const picture = req.body;
+        picturesDB.updatePicture({...picture, id: id})
+        res.send({...picture, id: id});
+    }
+    catch (error) {
+        res.status(400).send({error: error.message})
+    }
+})
+
+router.post('/photos', (req, res) => {
+    try{
+        const content = req.body;
+        const newPic = picturesDB.addNewPicture(content)
+        res.send(newPic);
+    }
+    catch (error) {
+        res.status(400).send({error: error.message})
+    }
+})
+
 
 module.exports = router;
