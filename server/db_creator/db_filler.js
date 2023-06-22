ef40ee271c7411c71411268b52542fbdf4329ff8
Cohen <itamar2.cohen@intel.com>
2023-06-22 15:46:46 +0300

    filler

--- server/db_creator/db_filler.js
+++ server/db_creator/db_filler.js
@@ -1,14 +1,95 @@
-async function insert_users(limit=20){
-    var users =  await fetch(
-        `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
-      ).then((response) => response.json());
+const pictures = require('../db_manager/pictures');
+const posts = require('../db_manager/posts');
+const user = require('../db_manager/user');
+const todos = require('../db_manager/todos');
+const comments = require('../db_manager/comments');
+const albums = require('../db_manager/albums');
 
+
+async function insert_users(limit=20){
+  const users_added = []
+      fetch(
+        `https://jsonplaceholder.typicode.com/users`
+      ).then((response) => response.json())
+      .then(res => {
+        for(let i = 0; i < res.length;i++){
+          res = await user.addUser(res[i]);
+          users_added.push(res)
+        }
+      })
+      console.log(users_added)
 }
 
 async function posts_filler(limit=20){
-
+  const users_added = []
+  fetch(
+    `https://jsonplaceholder.typicode.com/posts`
+  ).then((response) => response.json())
+  .then(res => {
+    for(let i = 0; i < res.length;i++){
+      res = await posts.addPost(res[i])
+      users_added.push(res)
+    }
+  })
+  console.log(users_added)
 }
 
 async function comments_filler(limit=20){
 
+  const users_added = []
+  fetch(
+    `https://jsonplaceholder.typicode.com/comments`
+  ).then((response) => response.json())
+  .then(res => {
+    for(let i = 0; i < res.length;i++){
+      res = await comments.addComment(res[i])
+      users_added.push(res)
+    }
+  })
+  console.log(users_added)
+
 }
+
+async function insert_albums(limit=20){
+  const users_added = []
+      fetch(
+        `https://jsonplaceholder.typicode.com/albums`
+      ).then((response) => response.json())
+      .then(res => {
+        for(let i = 0; i < res.length;i++){
+          res = await albums.addAlbum(res[i]);
+          users_added.push(res)
+        }
+      })
+      console.log(users_added)
+}
+
+async function insert_pictures(limit=20){
+  const users_added = []
+      fetch(
+        `https://jsonplaceholder.typicode.com/photos`
+      ).then((response) => response.json())
+      .then(res => {
+        for(let i = 0; i < res.length;i++){
+          res = await pictures.addNewPicture(res[i]);
+          users_added.push(res)
+        }
+      })
+      console.log(users_added)
+}
+
+async function insert_todos(limit=20){
+  const users_added = []
+      fetch(
+        `https://jsonplaceholder.typicode.com/todos`
+      ).then((response) => response.json())
+      .then(res => {
+        for(let i = 0; i < res.length;i++){
+          res = await todos.addTodo(res[i]);
+          users_added.push(res)
+        }
+      })
+      console.log(users_added)
+}
+
+
